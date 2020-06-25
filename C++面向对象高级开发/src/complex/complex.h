62a1cbf1ae7f58b554633f091d9acf0d893cc923
fenghaze <feng_haze@163.com>
2020-06-25 22:17:44 +0800

    add

--- C++面向对象高级开发/src/complex/complex.h
+++ C++面向对象高级开发/src/complex/complex.h
@@ -3,6 +3,23 @@
 
 
 
+class complex
+{
+private:
+    double re, im;
+    friend complex
+public:
+    complex(double r=0, double i =0): re(r), im(i) {}
+
+};
+
+complex::complex(/* args */)
+{
+}
+
+
+
+
 
 
 #endif
